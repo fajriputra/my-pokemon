e45db0fadeaf70cc0d5ef9d0961a8248c167c136
fajriputra <fajriwn27@gmail.com>
2024-08-23 22:57:00 +0700

    asd

--- app/pokemon/[name]/page.tsx
+++ app/pokemon/[name]/page.tsx
@@ -1,7 +1,7 @@
 import PokemonDetailWrapper from "@/components/detail";
 
 export async function generateStaticParams() {
-  const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=300");
   const data = await pokemons.json();
 
   return data.results?.map((rest: { name: string; url: string }) => ({
