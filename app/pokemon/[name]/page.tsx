a0b4d07ca2a46e179f25d603bfa01adcfb2ce869
fajriputra <fajriwn27@gmail.com>
2024-08-23 22:06:19 +0700

    test

--- app/pokemon/[name]/page.tsx
+++ app/pokemon/[name]/page.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export async function getPokemon(name: string) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const data = await res.json();
   return data;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,10 @@
 const nextConfig = {
   output: "export",
   images: {
+    unoptimized: true,
     domains: ["raw.githubusercontent.com"],
   },
+  trailingSlash: true, // Penting agar path di GitHub Pages dapat bekerja dengan baik
 };
 
 export default nextConfig;
